61f887cf3901220d51b155ecc600ad38353e3fa1
CreeGunning <creenike@gmail.com>
2021-04-02 22:29:51 +0100

    Nearly There

--- CL_SPORTS Queris for Business Requirements.sql
+++ CL_SPORTS Queris for Business Requirements.sql
@@ -1,53 +1,28 @@
 USE CL_SPORTS;
 
-SELECT Item_Dimension.BranchID, COUNT(CL_SPORTS_Fact_Table.OrderID) AS "Amount of Orders"
+SELECT Customers_Dimension.Fname, Customers_Dimension.Lname, CL_SPORTS_Fact_Table.OrderID, Timeline_Dimension.MonthText, DATEDIFF(Purchase_Orders_Dimension.ShippingDate, Timeline_Dimension.Date_o) AS "Processing Length (Days)"
 FROM CL_SPORTS_Fact_Table
-INNER JOIN Item_Dimension ON CL_SPORTS_Fact_Table.ItemID=Item_Dimension.ItemID
-GROUP BY BranchID
-Order By COUNT(CL_SPORTS_Fact_Table.OrderID) DESC;
-
-SELECT Timeline_Dimension.MonthID, sum(Item_Dimension.Price) AS "Total Sales", AVG(Item_Dimension.Price) AS "Average Sales"
-FROM CL_SPORTS_Fact_Table
-INNER JOIN Timeline_Dimension ON CL_SPORTS_Fact_Table.TimeID=Timeline_Dimension.TimeID 
-INNER JOIN Item_Dimension ON CL_SPORTS_Fact_Table.ItemID=Item_Dimension.ItemID
-WHERE Timeline_Dimension.MonthID="11" OR Timeline_Dimension.MonthID="12"
-GROUP BY MonthID
-ORDER BY SUM(Item_Dimension.Price) DESC;
-
-SELECT Item_Dimension.SportsCategory, COUNT(Item_Dimension.SportsCategory) AS "Amount of Sales"
-FROM CL_SPORTS_Fact_Table
-INNER JOIN Item_Dimension ON CL_SPORTS_Fact_Table.ItemID=Item_Dimension.ItemID
-GROUP BY SportsCategory
-ORDER BY COUNT(Item_Dimension.SportsCategory) DESC;
-
-SELECT CL_SPORTS_Fact_Table.OrderID, Timeline_Dimension.MonthText, DATEDIFF(Purchase_Orders_Dimension.DeliveredDate, Purchase_Orders_Dimension.ShippingDate) AS "Delivery Length (Days)"
-FROM CL_SPORTS_Fact_Table
+INNER JOIN Customers_Dimension ON CL_SPORTS_Fact_Table.CustomerID=Customers_Dimension.CustomerID
 INNER JOIN Purchase_Orders_Dimension ON CL_SPORTS_Fact_Table.OrderID=Purchase_Orders_Dimension.OrderID
 INNER JOIN Timeline_Dimension ON CL_SPORTS_Fact_Table.TimeID=Timeline_Dimension.TimeID
-WHERE Timeline_Dimension.MonthText="NOV" OR Timeline_Dimension.MonthText="DEC"
-ORDER BY MonthID;
+ORDER BY Timeline_Dimension.Year_o;
 
+SELECT Customers_Dimension.Fname, Customers_Dimension.Lname, CL_SPORTS_Fact_Table.OrderID, Timeline_Dimension.MonthText, DATEDIFF(Purchase_Orders_Dimension.DeliveredDate, Purchase_Orders_Dimension.ShippingDate) AS "Delivery Length (Days)"
+FROM CL_SPORTS_Fact_Table
+INNER JOIN Customers_Dimension ON CL_SPORTS_Fact_Table.CustomerID=Customers_Dimension.CustomerID
+INNER JOIN Purchase_Orders_Dimension ON CL_SPORTS_Fact_Table.OrderID=Purchase_Orders_Dimension.OrderID
+INNER JOIN Timeline_Dimension ON CL_SPORTS_Fact_Table.TimeID=Timeline_Dimension.TimeID
+ORDER BY Timeline_Dimension.Year_o;
 
-# Counts the amount of Assistant managers per branch
-# !!
-SELECT BNo, EmpNo, Fname, Lname, Role, COUNT(EmpNo)
-FROM employees
-WHERE role = 'Assistant Manager'
-GROUP BY BNo;
+SELECT CL_SPORTS_Fact_Table.OrderID, Purchase_Orders_Dimension.ExpressDeliveryCharge AS "Express Delivery Charge", Timeline_Dimension.MonthID, Purchase_Orders_Dimension.ExpressDeliveryCharge + Item_Dimension.Price AS "Total Cost of Order"
+FROM CL_SPORTS_Fact_Table
+INNER JOIN Purchase_Orders_Dimension ON CL_SPORTS_Fact_Table.OrderID=Purchase_Orders_Dimension.OrderID
+INNER JOIN Item_Dimension ON CL_SPORTS_Fact_Table.ItemID=Item_Dimension.ItemID
+INNER JOIN Timeline_Dimension ON CL_SPORTS_Fact_Table.TimeID=Timeline_Dimension.TimeID
+ORDER BY Timeline_Dimension.Year_o;
 
-#INNER Joining the tables CUSTOMERS and ORDERS to show CustomerNum, First Name, Last Name, StockNum, BNo
-# All customers who have ordered products from G02
-# !!
-SELECT customers.CustomerNum, customers.Fname, customers.Lname, orders.OrderNum, orders.BNo
-FROM customers
-INNER JOIN orders ON customers.CustomerNum=orders.CustomerNum AND orders.BNo='G02'
-ORDER BY CustomerNum;
-    
-#INNER Joining the tables STOCK, CUSTOMERS and ORDERS to show First Name, Last Name, Category, Model
-# All customers who ordered Kids products
-# !!
-SELECT customers.CustomerNum, customers.Fname, customers.Lname, orders.OrderNum, stock.Category, stock.Model
-FROM stock
-INNER JOIN orders ON stock.Model=orders.Model AND stock.Category='Kids'
-INNER JOIN customers ON orders.CustomerNum=customers.CustomerNum
-ORDER BY CustomerNum;
+SELECT CL_SPORTS_Fact_Table.OrderID, Item_Dimension.BranchID, Supplier_Dimension.PriceBoughtFor AS "Bought For", Item_Dimension.Price AS "Sold For",  Item_Dimension.Price - Supplier_Dimension.PriceBoughtFor AS "Total Profit of Order (excl. Delivery Charge)"
+FROM CL_SPORTS_Fact_Table
+INNER JOIN Item_Dimension ON CL_SPORTS_Fact_Table.ItemID=Item_Dimension.ItemID
+INNER JOIN Supplier_Dimension ON Item_Dimension.Supplier_StockID=Supplier_Dimension.Supplier_StockID
+ORDER BY BranchID;
